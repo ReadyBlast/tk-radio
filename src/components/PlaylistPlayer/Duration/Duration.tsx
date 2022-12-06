d50af6b4caee0a3917897b91c7d3ccb6591b1f5e
Alexey Ovchinnikov <88533698+ReadyBlast@users.noreply.github.com>
2022-12-07 02:37:26 +0400

    adding touch events

--- src/components/PlaylistPlayer/Duration/Duration.tsx
+++ src/components/PlaylistPlayer/Duration/Duration.tsx
@@ -49,6 +49,8 @@ const Duration: React.FC<TProps> = ({ setAudCurrentTime, inputBgChanger }) => {
         }}
         onMouseDown={() => dispatch(setDurationToggle(false))}
         onMouseUp={() => setAudCurrentTime()}
+        onTouchEnd={() => setAudCurrentTime()}
+        onTouchStart={() => dispatch(setDurationToggle(false))}
         value={currentTime}
         type="range"
         min="0"
